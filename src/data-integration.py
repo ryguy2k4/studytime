8cb4d67f49b18aed10ddfbf2cb023127edbd51b5
Ryan Sponzilli <ryguy2k4@gmail.com>
2025-09-05 11:45:09 -0500

    added tableau workbook; added progress and type column parsed from title.

--- src/data-integration.py
+++ src/data-integration.py
@@ -4,6 +4,7 @@ def prepare_data(filename, term):
     due_dates = []
     courses = []
     titles = []
+    pcts = []
 
     df = pd.read_json(filename)
 
@@ -14,16 +15,34 @@ def prepare_data(filename, term):
             subs = str(title).split(' ')
             due_dates.append(subs[1])
             courses.append(subs[2])
-            titles.append(" ".join(subs[3::]))
+            # Has a progress pct
+            if str(title).__contains__('%'):
+                titles.append(" ".join(subs[3:-1]))
+                pcts.append(subs[-1].strip('[').strip(']').strip('%'))
+            else:
+                titles.append(" ".join(subs[3::]))
+                pcts.append(None)
         # Does not have a due date
         else:
             subs = str(title).split(' ')
             due_dates.append(None)
             courses.append(subs[0])
-            titles.append(" ".join(subs[1::]))
+            # Has a progress pct
+            if str(title).__contains__('%'):
+                titles.append(" ".join(subs[1:-1]))
+                pcts.append(subs[-1].strip('[').strip(']').strip('%'))
+            else:
+                titles.append(" ".join(subs[1::]))
+                pcts.append(None)
     df['due_date'] = due_dates
     df['course'] = courses
     df['title'] = titles
+    df['progress'] = pcts
+
+    # interpret type
+    is_study = df['title'].str.contains('study', case=False) | df['title'].str.contains('review', case=False)
+    df.loc[is_study, 'type'] = 'study'
+    df.loc[~is_study, 'type'] = 'assignment'
 
     # interpret date format
     df['start_date'] = pd.to_datetime(df['start_date'])
@@ -49,7 +68,7 @@ def prepare_data(filename, term):
         df["week"] = df["start_date"].apply(lambda x: x.week - 34 + 1)
 
     # order dataframe
-    df = df[["term", "week", "day of week", "course", "duration", "start_date", "end_date", 'due_date', 'title']]
+    df = df[["term", "week", "day of week", "course", "duration", 'type', "start_date", "end_date", 'due_date', 'title', 'progress']]
 
     return df
 
@@ -68,5 +87,34 @@ fa24 = fa24[fa24['course'] != 'Research']
 sp25 = prepare_data("data/raw/sp25.json", 'sp25')
 sp25['course'] = sp25['course'].replace({"Chem": "CHEM 104/105", "CS": "CS 307", "Astro": "ASTR 405", "Geol": "GEOL 432", "RST": "RST 100"})
 
+# combine all semesters
 master = pd.concat([fa23, sp24, fa24, sp25])
+
+# add course group column
+ds_group = ['STAT 107', 'STAT 207', 'IS 477', 'CS 307', 'IS 467']
+astro_group = ['ASTR 210', 'ASTR 310', 'ASTR 405']
+gened_group = ['HIST 164', 'ANTH 103', 'ACES 179', 'RST 100']
+geol_group = ['GEOL 107', 'GEOL 208', 'GEOL 432', 'GEOL 436', 'GEOL 497']
+stem_foundations = ['MATH 241', 'MATH 257', 'PHYS 213/214', 'CHEM 102/103', 'CHEM 104/105', 'MATH 285', 'PHYS 225']
+
+def find_group(course: str):
+    if course in ds_group:
+        return 'ds'
+    elif course in astro_group:
+        return 'astro'
+    elif course in gened_group:
+        return 'gened'
+    elif course in geol_group:
+        return 'geol'
+    elif course in stem_foundations:
+        return 'stem'
+    elif course == 'Research':
+        return 'research'
+    else:
+        return 'other'
+
+master['group'] = master['course'].apply(lambda x: find_group(x))
+master = master[["term", "week", "day of week", 'group', "course", "duration", 'type', "start_date", "end_date", 'due_date', 'title', 'progress']]
+
+# save to csv
 master.to_csv("data/master/studytime.csv", index=False)
